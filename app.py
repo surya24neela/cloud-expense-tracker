2b106f4d4f0d1d206ef9b75af0adfa8025461419
Surya pavan <166729915+surya24neela@users.noreply.github.com>
2026-03-01 12:54:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
-print("Cloud Expense Tracker Running")
+from flask import Flask, request, jsonify
+from database import expenses
+
+app = Flask(__name__)
+
+@app.route("/expenses", methods=["POST"])
+def add_expense():
+    data = request.json
+    expenses.append(data)
+    return jsonify({"message": "Expense added"}), 201
+
+@app.route("/expenses", methods=["GET"])
+def list_expenses():
+    return jsonify(expenses), 200
+
+@app.route("/health", methods=["GET"])
+def health():
+    return jsonify({"status": "healthy"}), 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
