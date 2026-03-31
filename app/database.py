a419a046ece3e36b79bc1fadbfa50cdf2b23eb26
Surya pavan <166729915+surya24neela@users.noreply.github.com>
2026-03-31 12:38:20 +0530

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,1 +1,29 @@
-expenses = []
+import os
+import psycopg2
+
+DB_HOST = os.getenv("DB_HOST", "db")
+DB_NAME = os.getenv("DB_NAME", "expense_db")
+DB_USER = os.getenv("DB_USER", "postgres")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "postgres")
+
+def get_connection():
+    return psycopg2.connect(
+        host=DB_HOST,
+        database=DB_NAME,
+        user=DB_USER,
+        password=DB_PASSWORD
+    )
+
+def init_db():
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS expenses (
+            id SERIAL PRIMARY KEY,
+            title VARCHAR(255),
+            amount FLOAT
+        );
+    """)
+    conn.commit()
+    cur.close()
+    conn.close()
